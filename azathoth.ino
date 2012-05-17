ab218d4347eff44203aef2f74485217f78064009
Steve McGrath <smcgrath23@gmail.com>
2012-05-17 09:20:05 -0500

    Strip some unused/pointless code

--- azathoth.ino
+++ azathoth.ino
@@ -27,7 +27,6 @@ void setup() {
   lcd.clear();
   lcd.write("Initializing...");
   init_io();
-  delay(500);
   // leave the sonar disabled until we want it.
   sonarAction.disable();
   link.sendData(1, 0x99); // Let the BB know we're alive
@@ -56,12 +55,6 @@ void dispatch_packet(int length, byte* packet) {
   }
   memcpy(data, packet + 3, len);
   switch (cmd) {
-    case 0x01:
-      cmd_connect();
-      break;
-    case 0x02:
-      cmd_disconnect();
-      break;
     case 0x03:
       cmd_lcd(len, data);
       break;
@@ -85,36 +78,16 @@ void dispatch_packet(int length, byte* packet) {
 }
 
 void init_io() {
-  // Without knowing *why* we're booting/rebooting, let's just make sure
-  // the unstoppable 300-pound metal monster is NOT moving:
-  pinMode(P_ESTOP, OUTPUT);
-  digitalWrite(P_ESTOP, LOW);
-  
   pinMode(P_LED, OUTPUT);
   pinMode(P_SONAR_EN, OUTPUT);
   pinMode(P_SONAR_PW, INPUT);
   digitalWrite(P_LED, HIGH);
-  // Command a ranging cycle just to let the sensor calibrate
-  lcd.setPos(1,0);
-  lcd.write("Cal SONAR..");
-  sonar.ping();
-  lcd.write("done");
 }
 
 void update_sonar() {
   // Stub, since we can't just pass sonar.update() to TimedAction (grumble)
   sonar.update();
   return;
-}
-
-void cmd_connect() {
-  lcd.clear();
-  lcd.write("Connected");
-}
-
-void cmd_disconnect() {
-  lcd.clear();
-  lcd.write("Disconnected");
 }
 
 void cmd_lcd(int length, byte* data) {
